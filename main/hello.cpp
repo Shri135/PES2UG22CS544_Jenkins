d1af322fbe7653d1283fcd74833788e6f7645d40
Shri135 <shrinivaslm13@gmail.com>
2025-03-14 12:20:27 +0530

    Added Name

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,5 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   cout << "SRN: PES2UG22CS544 \n";
+  cout << "Name: Shrinivas L M \n";
   return 0;
 }
